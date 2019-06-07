307fb7a19574a7c1ab023f1227b55a7dbf1d4ac5
emjogal <emmajgale@hotmail.com>
2019-06-07 13:38:06 +0100

    sorting out script links

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+      $('.carousel').slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        draggable: false,
+        pauseOnHover: false
+      });
+    });
