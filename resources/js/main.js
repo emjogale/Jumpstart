680ad1a83b6e534d1d7b1c212052cc81999bea6a
emjogal <emmajgale@hotmail.com>
2019-06-08 19:32:19 +0100

    adding jQuery animated navbar

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,4 +7,21 @@ $(document).ready(function(){
         draggable: false,
         pauseOnHover: false
       });
-    });
+
+      var oldScrollDistance = 0;
+      $(window).scroll(function() {
+        var newScrollDistance = $(this).scrollTop();
+        if (newScrollDistance - oldScrollDistance >= 50) {
+          var navBarHeight = $('.navbar').css('height');
+          $('.navbar').animate({top: '-' + navBarHeight}, 150);
+          oldScrollDistance = newScrollDistance;
+        }
+       else if (oldScrollDistance - newScrollDistance >= 50) {
+          $('.navbar').animate({top: '0px'}, 150);
+          oldScrollDistance = newScrollDistance;
+        }
+      });
+
+   });
+
+  
