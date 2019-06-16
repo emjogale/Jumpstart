cef6f37fa778db535ee7cd0fa4ed07678bcb25b5
emjogal <emmajgale@hotmail.com>
2019-06-16 11:13:01 +0100

    final commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,27 +1,26 @@
 $(document).ready(function(){
       $('.carousel').slick({
-        slidesToShow: 1,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 4000,
         arrows: false,
         draggable: false,
+        pauseOnFocus: false,
         pauseOnHover: false
       });
 
       var oldScrollDistance = 0;
       $(window).scroll(function() {
         var newScrollDistance = $(this).scrollTop();
-        if (newScrollDistance - oldScrollDistance >= 50) {
+        if (newScrollDistance - oldScrollDistance > 50) {
           var navBarHeight = $('.navbar').css('height');
           $('.navbar').animate({top: '-' + navBarHeight}, 150);
           oldScrollDistance = newScrollDistance;
         }
-       else if (oldScrollDistance - newScrollDistance >= 50) {
+       else if (oldScrollDistance - newScrollDistance > 50) {
           $('.navbar').animate({top: '0px'}, 150);
           oldScrollDistance = newScrollDistance;
         }
-      });
+      }); // end of .scroll
 
-   });
-
-  
+   }); // end of .ready
